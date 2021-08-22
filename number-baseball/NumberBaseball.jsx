7d8546b5c6dd17239e2d5e87af3edf984c33a12c
sperr <sperr@ajou.ac.kr>
2021-08-22 09:45:50 +0900

    webpack에 대한 기본적인 이해

---
+++ number-baseball/NumberBaseball.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+function getNumbers() {
+  //숫자 4개를 겹치지 않고 랜덤하게 뽑는 함수
+}
+
+class NumberBaseball extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      result: "",
+      value: "",
+      answer: getNumbers(),
+      tries: [],
+    };
+  }
+  onSubmitForm = () => {};
+  onChangeInput = () => {};
+  render() {
+    return (
+      <>
+        <h1>{this.state.result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input
+            maxLength={4}
+            value={this.state.value}
+            onChange={this.onChangeInput}
+          />
+        </form>
+        <div>시도: {this.state.tries.length}</div>
+        <ul>
+          <li>
+            {["like", "like", "like", "like", "like"].map(() => {
+              return <li>like</li>;
+            })}
+          </li>
+        </ul>
+      </>
+    );
+  }
+}
+
+export default NumberBaseball;
